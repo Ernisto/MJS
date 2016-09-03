bbf65922db2b5762963c2ea70b832f36b0a4fce3
Ernist Bekboev <archinstone@gmail.com>
2016-09-03 12:16:43 +0600

    add department page

--- client/app/app.js
+++ client/app/app.js
@@ -23,6 +23,7 @@ import admin from './admin';
 import navbar from '../components/navbar/navbar.component';
 import footer from '../components/footer/footer.component';
 import main from './main/main.component';
+import department from './department/department.component';
 import constants from './app.constants';
 import util from '../components/util/util.module';
 import socket from '../components/socket/socket.service';
@@ -35,7 +36,7 @@ angular.module('mjsApp', [
     // ngMessages,
 
     // ngValidationMatch,
-    _Auth, account, admin, navbar, footer, main, constants, socket, util
+    _Auth, account, admin, navbar, footer, main, department, constants, socket, util
   ])
   .config(routeConfig)
   .run(function($rootScope, $location, Auth) {
