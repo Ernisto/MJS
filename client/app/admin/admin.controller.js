62950e04098e4f073a9c30f192f984ba206f6a2d
Ernist Bekboev <archinstone@gmail.com>
2016-09-13 23:03:05 +0600

    add i18n feature

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -2,13 +2,29 @@
 
 export default class AdminController {
   /*@ngInject*/
-  constructor(User) {
+  constructor(User, SweetAlert, $filter) {
     // Use the User $resource to fetch all users
     this.users = User.query();
+    this.SweetAlert = SweetAlert;
+    this.$filter = $filter;
   }
 
   delete(user) {
-    user.$remove();
-    this.users.splice(this.users.indexOf(user), 1);
+    this.SweetAlert.swal({
+      title: this.$filter('translate')('CONFIRMATION'),
+      text: this.$filter('translate')('USER_WARNING'),
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: this.$filter('translate')('DELETE_AGREE'),
+      cancelButtonText: this.$filter('translate')('CANCEL'),
+      closeOnConfirm: false
+    }, (isConfirmed) => {
+      if(isConfirmed) {
+        user.$remove();
+        this.users.splice(this.users.indexOf(user), 1);
+        swal(this.$filter('translate')('DELETED'), this.$filter('translate')('ACCOUNT_DELETED'), "success");
+      }
+    });
   }
 }
