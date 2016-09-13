10bc89f38b10d66998c5cd2227a15bd42b4cead8
Ernist Bekboev <archinstone@gmail.com>
2016-09-13 09:51:16 +0600

    filter journal abbreviation by URL

--- client/app/journal/journal.routes.js
+++ client/app/journal/journal.routes.js
@@ -4,7 +4,7 @@ export default function($stateProvider) {
   'ngInject';
   $stateProvider
     .state('journal', {
-      url: '/:journal',
+      url: '/{journal:mjen|mjss|mjal|reforma}',
       template: '<journal></journal>'
     });
 }
