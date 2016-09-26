2f178fd950e3b65cc21a4defa4f0563207d1417c
Ernist Bekboev <archinstone@gmail.com>
2016-09-27 00:54:19 +0600

    change journal url

--- client/app/journal/journal.routes.js
+++ client/app/journal/journal.routes.js
@@ -4,7 +4,7 @@ export default function($stateProvider) {
   'ngInject';
   $stateProvider
     .state('journal', {
-      url: '/:journal',
+      url: '/departments/:journal',
       template: '<journal></journal>'
     });
 }
