2a65ca785e8b61dbf2a6144ca1d747a895f2156b
Rita <ritochkina@gmail.com>
2016-09-27 14:54:07 +0600

    change url param

--- client/app/journal/journal.routes.js
+++ client/app/journal/journal.routes.js
@@ -4,7 +4,7 @@ export default function($stateProvider) {
   'ngInject';
   $stateProvider
     .state('journal', {
-      url: '/departments/:journal',
+      url: '/journal/:journal',
       template: '<journal></journal>'
     });
 }
