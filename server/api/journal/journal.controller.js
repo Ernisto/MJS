9122c2f1620e35434f84e76a01f7462c7d806d24
Ernist Bekboev <archinstone@gmail.com>
2016-10-18 01:15:22 +0600

    fix upsert operation

--- server/api/journal/journal.controller.js
+++ server/api/journal/journal.controller.js
@@ -98,8 +98,7 @@ export function upsert(req, res) {
   if(req.body._id) {
     delete req.body._id;
   }
-  return Journal.findOneAndUpdate(req.params.id, req.body, {upsert: true, setDefaultsOnInsert: true, runValidators: true}).exec()
-
+  return Journal.findOneAndUpdate({_id: req.params.id}, req.body, {upsert: true, setDefaultsOnInsert: true, runValidators: true}).exec()
     .then(respondWithResult(res))
     .catch(handleError(res));
 }
