53356dcc80344be9578ac960225cde9816cb9690
Ernist Bekboev <archinstone@gmail.com>
2017-02-17 00:24:24 +0600

    inject all existent modules to main module

--- client/app/app.js
+++ client/app/app.js
@@ -26,7 +26,10 @@ import _Auth from '../components/auth/auth.module';
 import Modal from '../components/modal/modal.service';
 import wysiwygEditor from '../components/wysiwyg-editor/wysiwyg-editor.directive';
 import account from './account';
-import admin from './admin';
+import admin from './admin/admin.component';
+import adminNavbar from './admin/admin-navbar/admin-navbar.component';
+import adminFooter from './admin/admin-footer/admin-footer.component';
+import adminMainSidebar from './admin/admin-main-sidebar/admin-main-sidebar.component';
 import navbar from '../components/navbar/navbar.component';
 import footer from '../components/footer/footer.component';
 import main from './main/main.component';
@@ -34,6 +37,10 @@ import constants from './app.constants';
 import util from '../components/util/util.module';
 import socket from '../components/socket/socket.service';
 import journal from './journal/journal.component';
+import journalService from './journal/service/journal.service';
+import archives from './archives/archives.component';
+import archive from './archives/archive/archive.component';
+import archiveService from './archives/service/archive.service';
 import dashboard from './dashboard/dashboard.component';
 import './scripts';
 
@@ -46,14 +53,31 @@ angular.module('mjsApp', [
   // ngMessages,
 
   // ngValidationMatch,
-  _Auth, Modal, wysiwygEditor, account, admin, navbar, footer, main, constants, socket, util, dashboard, journal
+  _Auth, Modal, wysiwygEditor, account, admin, adminNavbar, adminFooter, adminMainSidebar,
+  navbar, footer, main, constants, socket, util, dashboard, journal, journalService, archives, archive, archiveService
 ])
   .config(routeConfig)
-  .run(function ($rootScope, $location, Auth) {
+  .controller('RootController', function ($rootScope) {
+    'ngInject';
+
+    var vm = this;
+    vm.layout = 'default';
+
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+      vm.layout = toState.data.layout;
+      vm.bodyClass = toState.data.bodyClass ? toState.data.bodyClass : '';
+    });
+  })
+  .run(function ($rootScope, $location, $state, Auth) {
     'ngInject';
     // Redirect to login if route requires auth and you're not logged in
 
-    $rootScope.$on('$stateChangeStart', function (event, next) {
+    $rootScope.$on('$stateChangeStart', function (event, next, params) {
+      if (next.redirectTo) {
+        event.preventDefault();
+        $state.go(next.redirectTo, params, {location: 'replace'})
+      }
+
       Auth.isLoggedIn(function (loggedIn) {
         if (next.authenticate && !loggedIn) {
           $location.path('/login');
