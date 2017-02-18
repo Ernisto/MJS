7658d7e88b68997906378f5f941e8c1f1f238efd
Ernist Bekboev <archinstone@gmail.com>
2017-02-19 00:19:32 +0600

    modify Log structure

--- server/api/log/log.model.js
+++ server/api/log/log.model.js
@@ -5,10 +5,11 @@ import {registerEvents} from './log.events';
 
 var LogSchema = new mongoose.Schema({
   journal: { type : mongoose.Schema.ObjectId, ref : 'Journal' },
-  property: String,
+  property: { type : mongoose.Schema.ObjectId, ref : 'JournalProperty' },
   language: String,
   user: { type : mongoose.Schema.ObjectId, ref : 'User' },
-  timestamp: {type: Date, default: Date.now()}
+  timestamp: {type: Date, default: Date.now()},
+  content: String
 });
 
 registerEvents(LogSchema);
