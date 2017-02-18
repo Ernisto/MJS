0f20855c7ddfd94a84d833994327fcc75327c0c8
Ernist Bekboev <archinstone@gmail.com>
2017-02-19 00:23:16 +0600

    empty logs for development

--- server/config/seed.js
+++ server/config/seed.js
@@ -8,25 +8,28 @@ import User from '../api/user/user.model';
 import Journal from '../api/journal/journal.model';
 import JournalProperty from '../api/journal-property/journal-property.model';
 import Archive from '../api/archive/archive.model';
+import Log from '../api/log/log.model';
 
-User.find({}).remove()
-  .then(() => {
-    User.create({
-      provider: 'local',
-      name: 'Test User',
-      email: 'test@example.com',
-      password: 'test'
-    }, {
-      provider: 'local',
-      role: 'admin',
-      name: 'Admin',
-      email: 'admin@example.com',
-      password: 'admin'
-    })
-      .then(() => {
-        console.log('finished populating users');
-      });
-  });
+Log.find({}).remove().then(function () {
+  User.find({}).remove()
+    .then(() => {
+      User.create({
+        provider: 'local',
+        name: 'Test User',
+        email: 'test@example.com',
+        password: 'test'
+      }, {
+        provider: 'local',
+        role: 'admin',
+        name: 'Admin',
+        email: 'admin@example.com',
+        password: 'admin'
+      })
+        .then(() => {
+          console.log('finished populating users');
+        });
+    });
+});
 
 Journal.find({}).remove()
   .then(() => {
