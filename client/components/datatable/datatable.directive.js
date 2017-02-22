18cd96ae82d121e21b2b1dcf816d01e60bdbb410
Ernist Bekboev <archinstone@gmail.com>
2017-02-22 19:36:43 +0600

    extent datatable bindings

--- client/components/datatable/datatable.directive.js
+++ client/components/datatable/datatable.directive.js
@@ -4,17 +4,22 @@ import 'datatables.net';
 import 'datatables.net-bs';
 
 export default angular.module('mjsApp.datatable', [])
-  .directive('datatable', function($timeout) {
+  .directive('datatable', function ($timeout) {
     return {
       restrict: 'A',
       scope: {
         dtReady: '=',
-        dtConfig: '='
+        dtConfig: '=',
+        dtRendered: '='
       },
-      link: function(scope, element, attrs) {
-        scope.$watch('dtReady', function(newValue) {
-          if(newValue) {
-            $timeout(() => $(element).DataTable(scope.dtConfig));
+      link: function (scope, element, attrs) {
+        scope.$watch('dtReady', function (newValue) {
+          if (newValue) {
+            $timeout(() => $(element).DataTable(angular.merge(scope.dtConfig, {
+              "initComplete": function (settings, json) {
+                scope.dtRendered = true;
+              }
+            })));
 
             /** For debugging purpose compare quantity of tr's in tbody **/
             // console.log($(element).children('tbody').children('tr').length);
