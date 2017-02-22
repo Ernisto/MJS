1d72ba6f27b81eddc61495008676099089b52ee6
Ernist Bekboev <archinstone@gmail.com>
2017-02-22 19:38:58 +0600

    apply new routes to admin component

--- client/app/admin/admin.routes.js
+++ client/app/admin/admin.routes.js
@@ -15,7 +15,22 @@ export default function($stateProvider) {
     })
     .state('admin.monitoring', {
       url: '/monitoring',
-      template: require('./monitoring.html'),
-      stateName: 'monitoring'
+      template: '<admin-monitoring></admin-monitoring>'
     })
+    .state('admin.journals', {
+      url: '/journals',
+      template: '<admin-journals></admin-journals>'
+    })
+    .state('admin.archives', {
+      url: '/archives',
+      template: '<admin-archives></admin-archives>'
+    })
+    .state('admin.users', {
+      url: '/users',
+      template: '<admin-users></admin-users>'
+    })
+    .state('admin.logs', {
+      url: '/logs',
+      template: '<admin-logs></admin-logs>'
+    });
 }
